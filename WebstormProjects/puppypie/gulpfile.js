f41903bbf2a440e184ce4c3ff90625ef8d7890ca
Angelica Badillo <angelica_badillo@primaryusers-MacBook-Air.local>
2018-04-18 18:14:20 -0700

    added SASS wporking with Gulp

--- WebstormProjects/puppypie/gulpfile.js
+++ WebstormProjects/puppypie/gulpfile.js
@@ -1,10 +1,25 @@
-//CommonJS: Node.js Modules
+/*CommonJS: Module belongs to Node.js*/
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename'); //rename the output files
+const cleanCSS= require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
 
-const gulp = requiere('gulp');
-const sass = requiere('gulp-sass');
 
-gulp.task( 'sass', function(){
-    return gulp.src('./src/scss/main.scss')
+
+gulp.task( 'sass', () => {   //compilation task
+    return gulp.src('./src/scss/main.scss' ) //get the data
+        //.pipe( sourcemaps.init()) //Depuracion de codigo
         .pipe( sass().on('error', sass.logError))
-        .pipe( gulp.dest( './dist/scss' ));
-});
+        //gulp uses pipes, the result of one process is passed to another pipe so it goes to the next process.
+        //.pipe( rename ('all-styles.css'))
+        //.pipe( sourcemaps.write())
+        .pipe( gulp.dest( './dist/css'));
+} );
+
+gulp.task( 'default', ['sass']);
+
+
+
+
+
