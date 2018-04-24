e89415c18ac0ad271dbb4449b1457cd462dfc41a
Angelica Badillo <angelica_badillo@primaryusers-MacBook-Air.local>
2018-04-23 18:17:14 -0700

    Adding tasks

--- WebstormProjects/puppypie/gulpfile.js
+++ WebstormProjects/puppypie/gulpfile.js
@@ -6,7 +6,7 @@ const cleanCSS= require('gulp-clean-css');
 const sourcemaps = require('gulp-sourcemaps');
 const watch = require('gulp-watch');
 const util = require ('util');
-
+const fileinclude = require('gulp-file-include');
 
 
 gulp.task( 'sass', () => {   //compilation task
@@ -31,8 +31,46 @@ gulp.task( 'watch', () => {
 
 });
 
-gulp.task( 'build', [ 'saas' ] );
-gulp.task( 'default', ['sass', 'watch']);
+//Task to generate static pages from a template
+gulp.task( 'file-include', () => {
+    gulp.src(['./src/pages/wrappers/*.include.html'])
+        .pipe(fileinclude({
+            prefix : '@@',
+            basepath: '@file'
+        }))
+        .pipe(rename((path) => {
+            path.dirname += '/';
+            path.basename = path.basename.replace(".include", "");
+            path.extname = ".html"
+        }))
+        .pipe( gulp.dest( './dist/pages' ));
+});
+
+//Watch for changes in pages
+
+gulp.task( 'watch', () => {
+    watch( './src/pages/**/*.html', ( file ) => {
+        util.log( 'HTML file changed: ', file.path);
+        gulp.start( 'file-include', function() {
+
+        }).on( 'error', ( error ) => {
+            util.log( util.colors.red( 'Error' ), error.message );
+        });
+    });
+
+});
+
+//Copy static assets (images)
+gulp.task( 'copy-assets', () => {
+    return gulp.src('./src/img/*.png')
+        .pipe( gulp.dest('./dist/img'));
+
+});
+
+
+
+gulp.task( 'build', [ 'file-include','saas' ] );
+gulp.task( 'default', ['watch', 'build']);
 
 
 
